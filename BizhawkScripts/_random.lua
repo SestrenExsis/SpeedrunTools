a39565d1338c4043557c548b71f020aa81818d29
SestrenExsis <sestrenexsis@gmail.com>
2024-04-03 06:04:25 -0500

    Delete old Lua scripts

--- BizhawkScripts/_random.lua
+++
@@ -1,9 +0,0 @@
-
-require "SotnCore"
-require "SotnAutopilot"
-
-while true do
-    gui.clearGraphics()
-    SotnCore.draw_rng(128, 12)
-    emu.frameadvance()
-end
--- BizhawkScripts/autopilot.lua
+++
@@ -1,7 +0,0 @@
-
-require "SotnAutopilot"
-
-while true do
-    SotnAutopilot.check_for_macro_input()
-    emu.frameadvance()
-end
