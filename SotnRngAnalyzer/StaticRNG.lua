f965b7063855bfbb7b29d0e227f44d8f95362b9c
SestrenExsis <sestrenexsis@gmail.com>
2024-07-27 20:25:11 -0500

    Simplify injection logic

--- SotnRngAnalyzer/StaticRNG.lua
+++ SotnRngAnalyzer/StaticRNG.lua
@@ -35,7 +35,7 @@ P.Stage = {
     WARP_ROOM = 0x0E,
     BOSS_SUCCUBUS = 0x12,
     BOSS_CERBERUS = 0x16,
-    -- UNKNOWN_17 = 0x17,
+    MARIA_CUTSCENE_IN_CLOCK_ROOM = 0x17,
     BOSS_RICHTER = 0x18,
     BOSS_HIPPOGRYPH = 0x19,
     BOSS_DOPPELGANGER10 = 0x1A,
@@ -52,17 +52,17 @@ P.Stage = {
     CAVE = 0x25,
     ANTI_CHAPEL = 0x26,
     REVERSE_ENTRANCE = 0x27,
-    -- TODO(sestren): REVERSE_CASTLE_CENTER = 0x28,
+    REVERSE_CASTLE_CENTER = 0x28,
     REVERSE_CAVERNS = 0x29,
     REVERSE_COLOSSEUM = 0x2A,
     REVERSE_KEEP = 0x2B,
     NECROMANCY_LABORATORY = 0x2C,
     REVERSE_CLOCK_TOWER = 0x2D,
     REVERSE_WARP_ROOM = 0x2E,
-    -- TODO(sestren): BOSS_DARWKING_BAT = 0x35,
+    BOSS_DARWKING_BAT = 0x35,
     BOSS_GALAMOTH = 0x36,
     BOSS_AKMODAN_II = 0x37,
-    BOSS_SHAFT = 0x38,
+    BOSS_DRACULA = 0x38,
     BOSS_DOPPELGANGER40 = 0x39,
     BOSS_CREATURE = 0x3A,
     BOSS_MEDUSA = 0x3B,
@@ -78,178 +78,59 @@ P.stage_id = function()
     return result
 end
 
-P.injections = {
-    ['nop'] = { instruction = 0x00000000, mask = 0x0000 },
-    ['li v0, X'] = { instruction = 0x34020000, mask = 0x7FFF },
-    ['li a0, X'] = { instruction = 0x34040000, mask = 0x7FFF },
-    ['li a1, X'] = { instruction = 0x34050000, mask = 0x7FFF },
-    ['li s0, X'] = { instruction = 0x34100000, mask = 0x7FFF },
-    ['li s1, X'] = { instruction = 0x34110000, mask = 0x7FFF },
-    ['slti v0, v0, X'] = { instruction = 0x28420000, mask = 0x7FFF },
-    ['slti v0, s1, X'] = { instruction = 0x2A220000, mask = 0x7FFF },
-    ['sll v0, X'] = { instruction = 0x00021000, mask = 0x001F, multiplier = 64 },
-    ['sll v0, v1, X'] = { instruction = 0x00031000, mask = 0x001F, multiplier = 64 },
-    ['srl v0, $18'] = { instruction = 0x00021602, mask = 0x0000 },
-    ['andi s1, v0, X'] = { instruction = 0x30510000, mask = 0x7FFF },
-    ['addiu v0, X'] = { instruction = 0x24420000, mask = 0x7FFF },
-    ['addiu a0, v0, X'] = { instruction = 0x24440000, mask = 0x7FFF },
-    ['addiu a1, a0, X'] = { instruction = 0x24850000, mask = 0x7FFF },
-    ['la v0, X'] = { instruction = 0x24850000, mask = 0x7FFF },
-    ['lw a0, X(s3)'] = { instruction = 0x8E640000, mask = 0x7FFF },
-    ['lw v0, X(s3)'] = { instruction = 0x8E620000, mask = 0x7FFF },
-    ['lw v1, X(a1)'] = { instruction = 0x8CA30000, mask = 0x7FFF },
-    ['lw a1, X(a1)'] = { instruction = 0x8CA50000, mask = 0x7FFF },
-    ['copy'] = { instruction = 0x00000000, mask = 0xFFFFFFFF },
-}
-
-P.hooks = {
-    [P.Stage.PROLOGUE] = { -- jal $801B186C
-        ['Stage Nice RNG'] = { address = 0x801B1898, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.CASTLE_ENTRANCE] = { -- jal $801C184C, search for 0C070613
-        ['Stage Nice RNG'] = { address = 0x801C1878, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.CASTLE_ENTRANCE_2] = { -- jal $801B90BC, search for 0C06E42F
-        ['Stage Nice RNG'] = { address = 0x801B90E8, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.ALCHEMY_LABORATORY] = { -- jal $801B94D4 -- Also Slogra and Gaibon
-        ['Stage Nice RNG'] = { address = 0x801B9500, default = 0x00021602, injection = 'li v0, X', mask = 0x00 },
-    },
-    [P.Stage.MARBLE_GALLERY] = { -- jal $801C3788, search for 0C070DE2
-        ['Stage Nice RNG'] = { address = 0x801C37B4, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.WARP_ROOM] = { -- jal $801881E8, search for 0C06207A
-        ['Stage Nice RNG'] = { address = 0x80188214, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.OUTER_WALL] = { -- jal $801C19F0, search for 0C07067C
-        ['Stage Nice RNG'] = { address = 0x801C1A1C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_DOPPELGANGER10] = { -- jal $801B69BC, search for 0C06DA6F
-        ['Stage Nice RNG'] = { address = 0x801B69E8, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.OLROXS_QUARTERS] = { -- jal $801B8714, search for 0C06E1C5
-        ['Stage Nice RNG'] = { address = 0x801B8740, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.COLOSSEUM] = { -- jal $801B86D4, search for 0C06E1B5
-        ['Stage Nice RNG'] = { address = 0x801B8700, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_MINOTAUR_AND_WEREWOLF] = { -- jal $801A7670, search for 0C069D9C
-        ['Stage Nice RNG'] = { address = 0x801A769C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.LONG_LIBRARY] = { -- jal $801BF130, search for 0C06FC4C
-        ['Stage Nice RNG'] = { address = 0x801BF15C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.CLOCK_TOWER] = { -- jal $801AC6E0, search for 0C06B1B8 -- Also Karasuman
-        ['Stage Nice RNG'] = { address = 0x801AC70C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.CASTLE_KEEP] = { -- jal $801AD630, search for 0C06B58C
-        ['Stage Nice RNG'] = { address = 0x801AD65C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.ROYAL_CHAPEL] = { -- jal $801C5F88, search for 0C0717E2
-        ['Stage Nice RNG'] = { address = 0x801C5FB4, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_HIPPOGRYPH] = { -- jal $801A6BB4, search for 0C069AED
-        ['Stage Nice RNG'] = { address = 0x801A6BE0, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.UNDERGROUND_CAVERNS] = { -- jal $801CA5F0, search for 0C07297C
-        ['Stage Nice RNG'] = { address = 0x801CA61C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_SCYLLA] = { -- jal $801A6704, search for 0C0699C1
-        ['Stage Nice RNG'] = { address = 0x801A6730, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_SUCCUBUS] = { -- jal $80196F90, search for 0C065BE4
-        ['Stage Nice RNG'] = { address = 0x80196FBC, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.ABANDONED_MINE] = { -- jal $8019DE74, search for 0C06779D
-        ['Stage Nice RNG'] = { address = 0x8019DEA0, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_CERBERUS] = { -- jal $80196648, search for 0C065992
-        ['Stage Nice RNG'] = { address = 0x80196674, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.CATACOMBS] = { -- jal $801BB6A4, search for 0C06EDA9
-        ['Stage Nice RNG'] = { address = 0x801BB6D0, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_GRANFALOON] = { -- jal $801A55A0, search for 0C069568
-        ['Stage Nice RNG'] = { address = 0x801A55CC, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.CASTLE_CENTER] = { -- jal $80190C4C ???, search for ???
-        ['Stage Nice RNG'] = { address = 0x80190E78, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_RICHTER] = { -- jal $801A9B54, search for 0C06A6D5
-        ['Stage Nice RNG'] = { address = 0x801A9B80, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.REVERSE_KEEP] = { -- jal $801A24F4, search for 0C06893D
-        ['Stage Nice RNG'] = { address = 0x801A2520, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.REVERSE_CLOCK_TOWER] = { -- jal $801ACEA0, search for 0C06B3A8 -- Also Darkwing Bat
-        ['Stage Nice RNG'] = { address = 0x801ACECC, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.REVERSE_OUTER_WALL] = { -- jal $801A9C9C, search for 0C06A727
-        ['Stage Nice RNG'] = { address = 0x801A9CC8, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.FORBIDDEN_LIBRARY] = { -- jal $801A2B60, search for 0C068AD8
-        ['Stage Nice RNG'] = { address = 0x801A2B8C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.ANTI_CHAPEL] = { -- jal $801B462C, search for 0C06D18B
-        ['Stage Nice RNG'] = { address = 0x801B4658, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_MEDUSA] = { -- jal $80193198, search for 0C064C66
-        ['Stage Nice RNG'] = { address = 0x801931C4, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.DEATH_WINGS_LAIR] = { -- jal $801B6CF0, search for 0C06DB3C
-        ['Stage Nice RNG'] = { address = 0x801B6D1C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.REVERSE_COLOSSEUM] = { -- jal $801A6B40, search for 0C069AD0
-        ['Stage Nice RNG'] = { address = 0x801A6B6C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BLACK_MARBLE_GALLERY] = { -- jal $801B7324, search for 0C06DCC9
-        ['Stage Nice RNG'] = { address = 0x801B7350, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.NECROMANCY_LABORATORY] = { -- jal $801ACC04, search for 0C06B301
-       ['Stage Nice RNG'] = { address = 0x801ACC30, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.CAVE] = { -- jal $8019ABF4, search for 0C066AFD
-        ['Stage Nice RNG'] = { address = 0x8019AC20, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_DEATH] = { -- jal $801A1A80, search for 0C0686A0
-        ['Stage Nice RNG'] = { address = 0x801A1AAC, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.FLOATING_CATACOMBS] = { -- jal $801B3F50, search for 0C06CFD4
-        ['Stage Nice RNG'] = { address = 0x801B3F7C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.REVERSE_CAVERNS] = { -- jal $801CA1E4, search for 0C072879
-        ['Stage Nice RNG'] = { address = 0x801CA210, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.REVERSE_ENTRANCE] = { -- jal $801B3EB0, search for 0C06CFAC
-        ['Stage Nice RNG'] = { address = 0x801B3EDC, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_BEELZEBUB] = { -- jal $80195144, search for 0C0xxxxx
-        ['Stage Nice RNG'] = { address = 0x80195170, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_AKMODAN_II] = { -- jal $80xxxxxx, search for 0C0xxxxx
-        ['Stage Nice RNG'] = { address = 0x80195F00, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_CREATURE] = { -- jal $8019xxxx, search for 0C06xxxx
-        ['Stage Nice RNG'] = { address = 0x80198E38, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_TRIO] = { -- jal $8019xxxx, search for 0C06xxxx
-        ['Stage Nice RNG'] = { address = 0x8019A090, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_DOPPELGANGER40] = { -- jal $8019xxxx, search for 0C06xxxx
-        ['Stage Nice RNG'] = { address = 0x801B591C, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_GALAMOTH] = { -- jal $8019xxxx, search for 0C06xxxx
-        ['Stage Nice RNG'] = { address = 0x80199DC4, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_OLROX] = { -- jal $8019xxxx, search for 0C06xxxx
-        ['Stage Nice RNG'] = { address = 0x801BC108, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.BOSS_SHAFT] = { -- jal $8019xxxx, search for 0C06xxxx
-        ['Stage Nice RNG'] = { address = 0x801BC108, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    },
-    [P.Stage.REVERSE_WARP_ROOM] = { -- jal $8018xxxx, search for 0C06xxxx
-        ['Stage Nice RNG'] = { address = 0x8018A194, default = 0x00021602, injection = 'li v0, X', mask = 0xFF },
-    }
+P.addresses = {
+    [P.Stage.PROLOGUE] = 0x801B1898,
+    [P.Stage.CASTLE_ENTRANCE] = 0x801C1878,
+    [P.Stage.CASTLE_ENTRANCE_2] = 0x801B90E8,
+    [P.Stage.ALCHEMY_LABORATORY] = 0x801B9500,
+    [P.Stage.MARBLE_GALLERY] = 0x801C37B4,
+    [P.Stage.WARP_ROOM] = 0x80188214,
+    [P.Stage.OUTER_WALL] = 0x801C1A1C,
+    [P.Stage.BOSS_DOPPELGANGER10] = 0x801B69E8,
+    [P.Stage.OLROXS_QUARTERS] = 0x801B8740,
+    [P.Stage.COLOSSEUM] = 0x801B8700,
+    [P.Stage.BOSS_MINOTAUR_AND_WEREWOLF] = 0x801A769C,
+    [P.Stage.LONG_LIBRARY] = 0x801BF15C,
+    [P.Stage.CLOCK_TOWER] = 0x801AC70C,
+    [P.Stage.CASTLE_KEEP] = 0x801AD65C,
+    [P.Stage.ROYAL_CHAPEL] = 0x801C5FB4,
+    [P.Stage.BOSS_HIPPOGRYPH] = 0x801A6BE0,
+    [P.Stage.UNDERGROUND_CAVERNS] = 0x801CA61C,
+    [P.Stage.BOSS_SCYLLA] = 0x801A6730,
+    [P.Stage.BOSS_SUCCUBUS] = 0x80196FBC,
+    [P.Stage.ABANDONED_MINE] = 0x8019DEA0,
+    [P.Stage.BOSS_CERBERUS] = 0x80196674,
+    [P.Stage.CATACOMBS] = 0x801BB6D0,
+    [P.Stage.BOSS_GRANFALOON] = 0x801A55CC,
+    [P.Stage.CASTLE_CENTER] = 0x80190E78,
+    [P.Stage.MARIA_CUTSCENE_IN_CLOCK_ROOM] = 0x8018CF9C,
+    [P.Stage.BOSS_RICHTER] = 0x801A9B80,
+    [P.Stage.REVERSE_KEEP] = 0x801A2520,
+    [P.Stage.REVERSE_CLOCK_TOWER] = 0x801ACECC,
+    [P.Stage.REVERSE_OUTER_WALL] = 0x801A9CC8,
+    [P.Stage.FORBIDDEN_LIBRARY] = 0x801A2B8C,
+    [P.Stage.ANTI_CHAPEL] = 0x801B4658,
+    [P.Stage.BOSS_MEDUSA] = 0x801931C4,
+    [P.Stage.DEATH_WINGS_LAIR] = 0x801B6D1C,
+    [P.Stage.REVERSE_COLOSSEUM] = 0x801A6B6C,
+    [P.Stage.BLACK_MARBLE_GALLERY] = 0x801B7350,
+    [P.Stage.NECROMANCY_LABORATORY] = 0x801ACC30,
+    [P.Stage.CAVE] = 0x8019AC20,
+    [P.Stage.BOSS_DEATH] = 0x801A1AAC,
+    [P.Stage.FLOATING_CATACOMBS] = 0x801B3F7C,
+    [P.Stage.REVERSE_CAVERNS] = 0x801CA210,
+    [P.Stage.REVERSE_CASTLE_CENTER] = 0x801A0498,
+    [P.Stage.REVERSE_ENTRANCE] = 0x801B3EDC,
+    [P.Stage.BOSS_BEELZEBUB] = 0x80195170,
+    [P.Stage.BOSS_AKMODAN_II] = 0x80195F00,
+    [P.Stage.BOSS_CREATURE] = 0x80198E38,
+    [P.Stage.BOSS_TRIO] = 0x8019A090,
+    [P.Stage.BOSS_DOPPELGANGER40] = 0x801B591C,
+    [P.Stage.BOSS_DARWKING_BAT] = 0x801ACECC,
+    [P.Stage.BOSS_GALAMOTH] = 0x80199DC4,
+    [P.Stage.BOSS_OLROX] = 0x801BC108,
+    [P.Stage.BOSS_DRACULA] = 0x801A54E8,
+    [P.Stage.REVERSE_WARP_ROOM] = 0x8018A194,
 }
 
 P.global_sources = {
@@ -417,27 +298,12 @@ P.update = function()
     end
     -- Assign Evil RNG
     memory.write_u32_le(0x002224, BizMath.bor(0x34020000, P.fixed_evil_rng), 'BiosROM')
-    -- Assign Nice RNG, if it can be found in one the Stage hooks
-    if P.hooks[P.stage_id()] ~= nil then
-        for desc, hook in pairs(P.hooks[P.stage_id()]) do
-            local source = P.global_sources[desc]
-            -- Set value to inject to either the default value or the one given by the source
-            local injected_value = hook.default
-            if source == nil or source.type == 'vanilla' then
-                injected_value = hook.default
-            elseif hook.injection ~= nil and P.injections[hook.injection] ~= nil then
-                local rng_value = BizMath.band(
-                    P.fixed_nice_rng,
-                    P.injections[hook.injection].mask
-                )
-                injected_value = BizMath.bor(
-                    P.injections[hook.injection].instruction,
-                    rng_value
-                )
-            end
-            -- Write the injected value at the hook's address
-            mainmemory.write_u32_le(hook.address - 0x80000000, injected_value)
-        end
+    -- Assign Nice RNG, if it can be found in Stage memory
+    local address = P.addresses[P.stage_id()]
+    if address ~= nil then
+        local rng_value = BizMath.band(P.fixed_nice_rng, 0xFF)
+        local instruction = 0x34020000 | rng_value
+        mainmemory.write_u32_le(address - 0x80000000, instruction)
     end
     -- Update previous frame information
     P.prev_nice_index = P.nice_index
